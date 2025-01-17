0da62469b81b0a185e0eee0b0b36cc7a7730542c
Zach Nobles <zachnobles04@gmail.com>
2025-01-17 17:03:53 -0500

    added work in progress message thing to the home page

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,7 +3,8 @@ import "./header.css";
 
 const Header = () => {
     return <div className="header">
-        <p className="glyph header-text">Zach's Website</p>
+        <p className="glyph">Zach's Website</p>
+        <p className="glyph" style={{fontSize: "0.75rem"}}>(work in progress)</p>
     </div>;
 }
 
